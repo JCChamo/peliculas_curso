46df86b03ea62a7eab6bd9f49d1e8b5ab1263822
José Carlos <josecarlos.chamorro@petroprix.com>
2022-07-18 14:48:12 +0200

    Hasta vídeo 113

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -10,9 +10,12 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: CustomScrollView(
-          slivers: [_CustomAppBar()],
-          physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics())
-        ),
+            slivers: [
+              _CustomAppBar(),
+              SliverList(delegate: SliverChildListDelegate([_PosterAndTitle()]))
+            ],
+            physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics())),
       ),
     );
   }
@@ -38,6 +41,50 @@ class _CustomAppBar extends StatelessWidget {
             placeholder: AssetImage('assets/loading.gif'),
             image: NetworkImage('https://via.placeholder.com/500x300'),
             fit: BoxFit.cover),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final texTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              height: 150,
+              placeholder: AssetImage('asstes/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie-title',
+                  style: texTheme.headline5,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2),
+              Text('movie-originalTitle',
+                  style: texTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1),
+              Row(children: [
+                const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                const SizedBox(width: 5),
+                Text('movie.voteAverage', style: texTheme.caption)
+              ])
+            ],
+          )
+        ],
       ),
     );
   }
